2b90de2cffc7740e917677549c13ec4e737c8cb8
Solar <solar+git@lunar-dev.com>
2023-11-29 21:57:25 +0000

    :sparkles: feat: created a common lib for all libs

---
+++ Gelos-Common/src/main/java/com/lunar/dev/gelos/common/CommonPlugin.java
@@ -0,0 +1,17 @@
+package com.lunar.dev.gelos.common;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class CommonPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
