72d4b6db4bef1b39bfbf30beaee8592f7fd4cfdd
Solar <solar+git@lunar-dev.com>
2023-11-29 21:39:58 +0000

    :sparkles: feat: created the games lib plugin

---
+++ Gelos-Games/src/main/java/com/lunar/dev/gelos/games/gelosgames/GeloPlugin.java
@@ -0,0 +1,20 @@
+package com.lunar.dev.gelos.games.gelosgames;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class GeloPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+
+
+
+}
