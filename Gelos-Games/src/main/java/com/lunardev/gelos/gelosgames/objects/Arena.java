6da9f14094d91ae8364993dfd9c12dd4b242cfd9
Solar <solar+git@lunar-dev.com>
2023-11-30 22:03:09 +0000

    :recycle: refactor: changed package path

---
+++ Gelos-Games/src/main/java/com/lunardev/gelos/gelosgames/objects/Arena.java
@@ -0,0 +1,13 @@
+package com.lunardev.gelos.gelosgames.objects;
+
+import com.lunardev.gelos.common.objects.CuboidRegion;
+
+public class Arena {
+
+    protected CuboidRegion region;
+
+    protected Arena() {
+    }
+
+
+}
