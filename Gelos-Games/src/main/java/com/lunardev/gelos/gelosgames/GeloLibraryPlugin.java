c2ccca2395ed44bceab454eacce49108d056a372
Solar <solar+git@lunar-dev.com>
2023-12-08 02:07:44 +0000

    :sparkles: feat: added a plugin file to load the library on a server
    Took 6 minutes

---
+++ Gelos-Games/src/main/java/com/lunardev/gelos/gelosgames/GeloLibraryPlugin.java
@@ -0,0 +1,10 @@
+package com.lunardev.gelos.gelosgames;
+
+import com.lunardev.gelos.common.CommonPlugin;
+
+public final class GeloLibraryPlugin extends CommonPlugin {
+    @Override
+    public void onStart() {
+        // Plugin startup logic
+    }
+}
