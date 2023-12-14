76b1d39077ce891424a5383054aaf4527ac79874
Solar <solar+git@lunar-dev.com>
2023-12-14 18:54:32 +0000

    :sparkles: added Cancel timer events
    Took 9 minutes

---
+++ Gelos-Games/src/main/java/com/lunardev/gelos/gelosgames/events/GameEndTimerCancelEvent.java
@@ -0,0 +1,31 @@
+package com.lunardev.gelos.gelosgames.events;
+
+import com.lunardev.gelos.gelosgames.managers.GameManager;
+import lombok.Getter;
+import org.bukkit.event.Event;
+import org.bukkit.event.HandlerList;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * This event is called when the end timer has been cancelled.
+ */
+public class GameEndTimerCancelEvent extends Event {
+
+    private static final HandlerList HANDLER_LIST = new HandlerList();
+
+    public static HandlerList getHandlerList() {
+        return HANDLER_LIST;
+    }
+
+    @Getter
+    private GameManager gameManager;
+
+    public GameEndTimerCancelEvent(GameManager manager) {
+        this.gameManager = manager;
+    }
+
+    @Override
+    public @NotNull HandlerList getHandlers() {
+        return HANDLER_LIST;
+    }
+}
