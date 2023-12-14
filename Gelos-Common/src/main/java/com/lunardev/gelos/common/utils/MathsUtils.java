69a09ca81e7555b0bb6c83c55edd8ccb237f557c
Solar <solar+git@lunar-dev.com>
2023-12-14 11:46:54 +0000

    :sparkles: feat: added maths isBetween function
    Took 55 seconds

---
+++ Gelos-Common/src/main/java/com/lunardev/gelos/common/utils/MathsUtils.java
@@ -0,0 +1,10 @@
+package com.lunardev.gelos.common.utils;
+
+public class MathsUtils {
+
+    public static boolean isBetween(int x, int lower, int upper) {
+        return lower <= x && x <= upper;
+    }
+
+
+}
