87329ce56192c6ceba63876e6d6b7b90203e94d4
Solar <solar+git@lunar-dev.com>
2023-12-14 18:54:45 +0000

    :sparkles: added couple events at the end
    Took 14 seconds

--- Gelos-Games/src/main/java/com/lunardev/gelos/gelosgames/managers/GameManager.java
+++ Gelos-Games/src/main/java/com/lunardev/gelos/gelosgames/managers/GameManager.java
@@ -1,9 +1,6 @@
 package com.lunardev.gelos.gelosgames.managers;
 
-import com.lunardev.gelos.gelosgames.events.GameEndEvent;
-import com.lunardev.gelos.gelosgames.events.GameStartEvent;
-import com.lunardev.gelos.gelosgames.events.GameStartTimerEvent;
-import com.lunardev.gelos.gelosgames.events.GameStartTimerFinishEvent;
+import com.lunardev.gelos.gelosgames.events.*;
 import com.lunardev.gelos.gelosgames.timers.GameEndTimer;
 import com.lunardev.gelos.gelosgames.timers.GameStartTimer;
 import lombok.AccessLevel;
@@ -12,6 +9,7 @@ import lombok.Setter;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 @Getter
@@ -38,6 +36,11 @@ public class GameManager implements Listener {
     @Setter
     private int endTimer = 10;
 
+    @Setter
+    private int minPlayers = 2;
+    @Setter
+    private int maxPlayers = 8;
+
     public GameManager(JavaPlugin plugin) {
         this.plugin = plugin;
         plugin.getServer().getPluginManager().registerEvents(this, plugin);
@@ -50,12 +53,13 @@ public class GameManager implements Listener {
 
     public void startTimer() {
         new GameStartTimerEvent(this).callEvent();
+        new GameStartTimer(plugin, this, startTimer).startTimer();
         state = GameState.START_TIMER;
     }
 
     @EventHandler
     public void onGameStartTimer(GameStartTimerEvent event) {
-        new GameStartTimer(plugin, this, startTimer).startTimer();
+        startTimer();
     }
 
     @EventHandler
@@ -74,8 +78,32 @@ public class GameManager implements Listener {
     public void onPlayerJoin(PlayerJoinEvent event) {
         if (joinOnConnect
                 && state == GameState.WAITING
-                && !event.getPlayer().hasPermission(bypassPermission)) {
+                && !event.getPlayer().hasPermission(bypassPermission)
+                && playerManager.getPlayers().size() < maxPlayers) {
             playerManager.addPlayer(event.getPlayer());
+        }
+    }
+
+    @EventHandler
+    public void onPlayerLeave(PlayerQuitEvent event) {
+        playerManager.removePlayer(event.getPlayer());
+    }
+
+    @EventHandler
+    public void onPlayerJoinGame(PlayerJoinedGameEvent event) {
+        if (state == GameState.WAITING) {
+            if (playerManager.getPlayers().size() >= minPlayers) {
+                new GameStartTimerEvent(this).callEvent();
+            }
+        }
+    }
+
+    @EventHandler
+    public void onPlayerLeftGame(PlayerLeftGameEvent event) {
+        if (state == GameState.WAITING) {
+            if (playerManager.getPlayers().size() < minPlayers) {
+                new GameStartTimerCancelEvent(this).callEvent();
+            }
         }
     }
 }
